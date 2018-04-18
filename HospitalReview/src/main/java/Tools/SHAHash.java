97a5ae6d179c0f38457a9a3c73fd92bc5f37b7df
hai07890 <hai06780@gmail.com>
2018-04-19 04:45:28 +0700

    Change to Maven, new Feature: email, sha256hash

---
+++ HospitalReview/src/main/java/Tools/SHAHash.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tools;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ *
+ * @author hai06
+ */
+public class SHAHash {
+
+    public static String hash(String data) {
+        return DigestUtils.sha256Hex(data);
+    }
+    
+    public static void main(String args[]) {
+        System.out.print(SHAHash.hash("123456"));
+    }
+}
